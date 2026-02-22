dc61dd113ee2097df110af359bc53d6eddb2c5f9
rukshangithub <rukshan.athauda@newcastle.edu.au>
2026-02-22 18:50:19 +0000

    Added .ignore

--- demo/src/main/java/com/example/StudentDAOImpl.java
+++ demo/src/main/java/com/example/StudentDAOImpl.java
@@ -3,7 +3,6 @@ package com.example;
 import java.sql.PreparedStatement;
 import java.io.File;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
